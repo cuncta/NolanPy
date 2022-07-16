90ae5ed0b0868a867af0b51e0eb5e4ca7c2356e7
SV <simone.vadilonga@helmholtz-berlin.de>
2022-07-16 09:54:49 +0200

    add test version

---
+++ Npy_test.py
@@ -0,0 +1,8 @@
+from Nlib import NolanPy
+
+N = NolanPy("https://docs.google.com/spreadsheets/d/11T2xbPFjlk1UI3NbdFFFGPQioUYW1i9S6bakjBpuovY/edit#gid=0")
+
+N.load_data(force=False, verbose=True)
+N.plot(show=True)
+N.send_email(send=False)
+   
