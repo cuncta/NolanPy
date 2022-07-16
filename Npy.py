1828199fa13f058c3dd745cc54fd2a8596a0ac70
SV <simone.vadilonga@helmholtz-berlin.de>
2022-07-16 09:54:59 +0200

    cleanup

--- Npy.py
+++ Npy.py
@@ -1,24 +1,9 @@
-import gspread
-import string
-import matplotlib.pyplot as plt
-
-
 from Nlib import NolanPy
-
-
-#gc = gspread.oauth()
-
-
-#sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/11T2xbPFjlk1UI3NbdFFFGPQioUYW1i9S6bakjBpuovY/edit#gid=0")
-
-
-
 
 N = NolanPy("https://docs.google.com/spreadsheets/d/11T2xbPFjlk1UI3NbdFFFGPQioUYW1i9S6bakjBpuovY/edit#gid=0")
 
-N.load_data(force=False, verbose=True)
-
+N.load_data(force=True, verbose=True)
 N.plot(show=False)
-N.send_email2()
+N.send_email(send=True)
    
 
