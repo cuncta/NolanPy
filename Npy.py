b1b26485b9b54430c65de8c5a0c3633cf060fd0a
SV <simone.vadilonga@helmholtz-berlin.de>
2022-07-16 13:30:28 +0200

    add gloriana to delivery list

--- Npy.py
+++ Npy.py
@@ -4,6 +4,7 @@ N = NolanPy("https://docs.google.com/spreadsheets/d/11T2xbPFjlk1UI3NbdFFFGPQioUY
 
 N.load_data(force=True, verbose=True)
 N.plot(show=False)
-N.send_email(send=True)
+N.send_email(receiver_address='simnur.shared@gmail.com',send=True)
+N.send_email(receiver_address='gloriana.rangone@gmail.com',send=True)
    
 
