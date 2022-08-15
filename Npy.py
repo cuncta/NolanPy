819a7d682b232ec9058ef95652485b8e89d04dca
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2022-08-15 08:24:50 +0200

    change verbose

--- Npy.py
+++ Npy.py
@@ -2,7 +2,7 @@ from Nlib import NolanPy
 
 N = NolanPy("https://docs.google.com/spreadsheets/d/11T2xbPFjlk1UI3NbdFFFGPQioUYW1i9S6bakjBpuovY/edit#gid=0")
 
-N.load_data(force=True, verbose=True)
+N.load_data(force=True, verbose=False)
 N.plot(show=False)
 N.send_email(receiver_address='simnur.shared@gmail.com',send=True)
 N.send_email(receiver_address='gloriana.rangone@gmail.com',send=True)
--- Npy_test.py
+++ Npy_test.py
@@ -2,7 +2,7 @@ from Nlib import NolanPy
 
 N = NolanPy("https://docs.google.com/spreadsheets/d/11T2xbPFjlk1UI3NbdFFFGPQioUYW1i9S6bakjBpuovY/edit#gid=0")
 
-N.load_data(force=False, verbose=True)
+N.load_data(force=True, verbose=False)
 N.plot(show=True)
 N.send_email(receiver_address='simone.vadilonga@gmail.com', send=False)
    
